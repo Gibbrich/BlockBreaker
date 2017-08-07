7bdf45731ea7ba765fcddb445262431913c12c08
Arthur Dvurechenskiyi <dvurechenskiyi@cdc.ru>
2017-08-07 18:20:03 +0300

    Initial commit

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private static MusicPlayer instance;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
